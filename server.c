09a6e89754c9abbe78b017655b4fbbc6b4538dfe
Hackpaul <fpaulraj2007@gmail.com>
2026-06-29 21:57:10 +0530

    feat: Added multi routing and broadcasting-- Starting stage logic

--- server.c
+++ server.c
@@ -27,9 +27,44 @@ POLL :
 #define PORT 8080
 #define BACKLOG 5
 #define MAX_CLIENTS 10
-void close_fd(int fd_1){
-    close(fd_1);
+#define CLIENT_FD_STARTER 1
+
+
+
+struct parser_array {
+
+int slot;
+char msg[100];
+
+};
+
+void close_fd(int fd){
+    close(fd);
     }
+
+
+void parser(char *buffer,int buffer_size,struct parser_array *pass){
+  char *id=strtok(buffer," ");
+  if(id !=NULL){
+  pass->slot=atoi(id);
+  }
+  char *msg=strtok(NULL," ");
+  if(msg!=NULL){
+  strcpy(pass->msg,msg);
+  }
+  }
+
+
+void broadcast(char *buffer,int size,struct pollfd *fds_no){
+   int i;
+   for(i=CLIENT_FD_STARTER;i<MAX_CLIENTS;i++){
+   if(fds_no[i].fd!=-1){
+   send(fds_no[i].fd,buffer,size, 0);
+   }
+   }
+   }
+
+
 
 int main() {
     int sfd, cfd;
@@ -37,10 +72,11 @@ int main() {
     int i,j,activity;
     struct sockaddr_in server_addr, client_addr;
     struct pollfd fds[MAX_CLIENTS];
+    struct parser_array client_array;
     socklen_t client_len = sizeof(client_addr);
     char buffer[1024];
     char reply[1024];
-   
+    char broadcast_message[1024] , brodcast_message_for_new_client[40];
     // Creation of FD -- server fd
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1) {
@@ -70,7 +106,7 @@ int main() {
     listen(sfd,BACKLOG);
     }
 
-    // setting the other fds to -1in poll struct
+    // setting the other fds to -1 --  for ignorance
     for(i=1;i<MAX_CLIENTS;i++){
     fds[i].fd = -1;
     }
@@ -82,8 +118,7 @@ int main() {
 
 
     while(1){
-    
-    
+ 
     activity=poll(fds,MAX_CLIENTS,-1);  // Passing fds struct 
 
     if(activity<0){
@@ -100,36 +135,50 @@ int main() {
     }else{
     printf(" Accepted and fd created! \n");
     }
-   
+
     for(i=1;i<MAX_CLIENTS;i++){
     if(fds[i].fd == -1){
     fds[i].fd=cfd;
     fds[i].events=POLLIN;
     printf("FD = %d created \n",i);
-    nfds+=1;
+
+    snprintf(brodcast_message_for_new_client,sizeof(brodcast_message_for_new_client),"brodcast -- Clieant connected no : %d",i);
+    broadcast(brodcast_message_for_new_client,sizeof(brodcast_message_for_new_client),fds);
+
     break;
     }
     }
     }//sfd function
-    
+
     for(i=1;i<MAX_CLIENTS;i++){
     if(fds[i].fd == -1){ 
     continue;
     }else if(fds[i].revents & POLLIN){
      memset(buffer, 0, sizeof(buffer)); // clear the bucket
     recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
-    printf("Peer sent: %s\n", buffer);
-    printf("Enter to reply:\n");
+    printf("%d--Peer sent: %s\n",i, buffer);
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+
+    parser(buffer,sizeof(buffer),&client_array);
+    if(client_array.slot==0){
+    strcpy(broadcast_message,client_array.msg);
+    broadcast(broadcast_message,sizeof(broadcast_message),fds);
+    }else if(client_array.slot<MAX_CLIENTS && client_array.slot>=CLIENT_FD_STARTER){
+    send(fds[client_array.slot].fd,client_array.msg,sizeof(client_array.msg), 0);
+    }else{fprintf(stderr,"invalid fd is given\n");}
+
+
     fgets(reply,sizeof(reply),stdin);
     if(!strcmp(reply,"exit\n")){
     printf("Closing...\n");
     for(j=0;j<MAX_CLIENTS;j++){
     if(fds[j].fd > -1){
     close_fd(fds[j].fd);
-    
+
     }
     } // fd closing loop for EXIT reply 
-   
+
     printf("Successfully closed\n");
     exit(EXIT_SUCCESS);
     }// EXIT string check  
